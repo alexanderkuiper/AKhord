9c5508b30d1bb1643645f806e1f71fe52e75bc53
Alex Kuiper <kuiperalexander@gmail.com>
2022-08-20 15:31:05 +0800

    added icon, refactor dir structure. TODO: add history, add chord GUI

--- src/main/java/nl/miwgroningen/se/ch9/alex/controller/App.java
+++ src/main/java/nl/miwgroningen/se/ch9/alex/controller/App.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import nl.miwgroningen.se.ch9.alex.database.DBaccess;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * JavaFX App
@@ -15,14 +17,20 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    private static DBaccess dBaccess = new DBaccess("akkoord", "user", "password");
-
+    private static final DBaccess DBACCESS = new DBaccess(
+            "akkoord", "user", "password");
+    private final Image ICON = new Image(Objects.requireNonNull(getClass().getResource(
+            "/images/AKhordIcon.png")).toExternalForm());
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.getIcons().add(ICON);
+        stage.setTitle("AKhord");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -36,12 +44,12 @@ public class App extends Application {
 //    }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/nl/miwgroningen/se/ch9/alex/" + fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/view/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
     public static DBaccess getdBaccess() {
-        return dBaccess;
+        return DBACCESS;
     }
 
     public static void main(String[] args) {
