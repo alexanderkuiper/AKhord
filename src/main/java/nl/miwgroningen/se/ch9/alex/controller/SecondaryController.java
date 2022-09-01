ef690afc08a77a7d63805ae8377988d00009492e
Alex Kuiper <kuiperalexander@gmail.com>
2022-09-01 21:04:48 +0200

    trying to figure out iteration in order to create the chordSuggestions. How to adhere to the set restrictions?

--- src/main/java/nl/miwgroningen/se/ch9/alex/controller/SecondaryController.java
+++ src/main/java/nl/miwgroningen/se/ch9/alex/controller/SecondaryController.java
@@ -2,6 +2,8 @@ package nl.miwgroningen.se.ch9.alex.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -30,17 +32,59 @@ public class SecondaryController implements Initializable {
         Akkoord chord = Akkoord.pullChordFromDatabase(CHORD_POSITION, false);
         akkoordLabel.setText(chord.toString());
         notenLabel.setText(chord.geefTonenInAkkoord().toString());
+//        getPossibleUkeChords(populateChordGrid(chord));
         populateChordGrid(chord);
-
     }
 
-    private void populateChordGrid(Akkoord chord) {
+    private HashMap populateChordGrid(Akkoord chord) {
+        int noteIndex = 0;
+//        int chordNoteIndex = 0;
+        String[] ukuleleStrings = {"G", "C", "E", "A"};
+        ArrayList<String> chordNotes = chord.geefTonenInAkkoord();
         ChromaticScale notesList = new ChromaticScale(chord.getToon());
-        // for each note in chord:
-        // if (note equals noteslist[i]) {
-        // display X on the desired coordinate }
-        // else (increment i and display x on its coordinate
+        HashMap<String, HashMap> ukuleleHashMap = new HashMap<>();
 
+        for (int stringIndex = 0; stringIndex < ukuleleStrings.length; stringIndex++) {
+            ArrayList<String> stringNotes = selectUkuleleString(ukuleleStrings[stringIndex]);
+            HashMap<String, Integer> stringHashMap = new HashMap<>();
+            System.out.println();
+            for (int chordNoteIndex = 0; chordNoteIndex < chordNotes.size(); chordNoteIndex++) {
+                for (String stringNote : stringNotes) {
+                    if (stringNote.equals(chordNotes.get(chordNoteIndex))) {
+                        stringHashMap.put(chordNotes.get(chordNoteIndex), stringNotes.indexOf(stringNote));
+                        System.out.printf("Checking note %s in chord %s for a match on the %s-string. ", chordNotes.get(chordNoteIndex), chord, ukuleleStrings[stringIndex]);
+                        System.out.printf("The matching position is: %s\n", stringNotes.indexOf(stringNote));
+                        System.out.printf("Hashmap contains: %s\n", stringHashMap);
+                    }
+                }
+                ukuleleHashMap.put(ukuleleStrings[stringIndex], stringHashMap);
+            }
+            System.out.println("This is the complete set of hashmaps: " + ukuleleHashMap);
+        }
+        return ukuleleHashMap;
+    }
+
+    private ArrayList<String> selectUkuleleString(String note) {
+        return new ChromaticScale(note).getNotesList().fillArray();
+    }
+
+    private void getPossibleUkeChords(HashMap<String, HashMap> buildingBlocks) {
+        HashMap<String, Integer> chordSuggestion = new HashMap<>();
+        String[] ukuleleStrings = {"G", "C", "E", "A"};
+        String[] chordNotes = {"C", "E", "G"};
+        int i = 0;
+        int j = 0;
+//        chordSuggestion.put("G", 0);
+//        chordSuggestion.put("C", 0);
+//        chordSuggestion.put("E", 0);
+//        chordSuggestion.put("A", 3);
+        buildingBlocks.get(ukuleleStrings[i]).get(chordNotes[j]);
+
+        }
+
+
+    private Boolean isUkeChordValid() {
+        return true;
     }
 
 }
---
+++ src/main/java/nl/miwgroningen/se/ch9/alex/model/UkuleleString.java
@@ -0,0 +1,11 @@
+package nl.miwgroningen.se.ch9.alex.model;
+
+/**
+ * @author Alex Kuiper <al.kuiper@st.hanze.nl>
+ * <p>
+ * {wat doet dit?}
+ */
+public interface UkuleleString {
+
+
+}
