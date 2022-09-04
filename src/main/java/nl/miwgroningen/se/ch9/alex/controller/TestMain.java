7e586c8951e647a0edf917722dc07e6377df37ed
Alex Kuiper <kuiperalexander@gmail.com>
2022-09-04 14:21:35 +0200

    still experimenting with chord possibility generator

--- src/main/java/nl/miwgroningen/se/ch9/alex/controller/TestMain.java
+++ src/main/java/nl/miwgroningen/se/ch9/alex/controller/TestMain.java
@@ -1,9 +1,5 @@
 package nl.miwgroningen.se.ch9.alex.controller;
 
-import nl.miwgroningen.se.ch9.alex.database.AkkoordKeuzeDAO;
-import nl.miwgroningen.se.ch9.alex.database.DBaccess;
-import nl.miwgroningen.se.ch9.alex.model.CircularLinkedList;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,55 +39,67 @@ public class TestMain {
 //
 //        dBaccess.closeConnection();
 
+
         // test creating all possible combinations of 4 arrays (1 per string) containing 3 notes of a chord
-        int[] gString = {5, 9, 0};
-        int[] cString = {0, 4, 7};
-        int[] eString = {8, 0, 3};
-        int[] aString = {3, 7, 10};
+//        int[] gString = {5, 9, 0};
+//        int[] cString = {0, 4, 7};
+//        int[] eString = {8, 0, 3};
+//        int[] aString = {3, 7, 10};
 
         List<List<Integer>> allStrings = new ArrayList<>();
         allStrings.add(List.of(5, 9, 0));
         allStrings.add(List.of(0, 4, 7));
         allStrings.add(List.of(8, 0, 3));
         allStrings.add(List.of(3, 7, 10));
-        List<List<Integer>> result = new ArrayList<>();
+        List<Integer[]> results = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-        generatePermutations(allStrings, result, 0, current);
 
-//        System.out.println(Arrays.toString(allStrings.toArray()));
-        System.out.println(Arrays.toString(result.toArray()));
-
-
-        List<Integer> chordSuggestions = new ArrayList<>();
-//            // test print source arrays
-//        System.out.println(Arrays.deepToString(allStrings));
-//        System.out.println(Arrays.toString(allStrings[0]));
-//        System.out.println(allStrings[0][1]);
-//             test print result arrays
-//        System.out.println(Arrays.deepToString(chordSuggestions.toArray()));
-//        for (int i = 0; i < allStrings.length; i++) {
-//            for (int j = 0; j < allStrings[i].length; j++) {
-//            }
-//        List<Integer[]> allStrings = new ArrayList<>();
-//        allStrings.add(gString);
-//        allStrings.add(cString);
-//        allStrings.add(eString);
-//        allStrings.add(aString);
+        generateChordsList(allStrings, results, 0, current);
+        for (Integer[] result : results) {
+            System.out.println(Arrays.toString(result));
         }
 
-    public static void generatePermutations(List<List<Integer>> lists, List<List<Integer>> result, int depth, List<Integer> current) {
+        List<List<String>> allStringsString = new ArrayList<>();
+        allStringsString.add(List.of("5", "9", "0"));
+        allStringsString.add(List.of("0", "4", "7"));
+        allStringsString.add(List.of("8", "0", "3"));
+        allStringsString.add(List.of("3", "7", "10"));
+        List<String> resultString = new ArrayList<>();
+        String currentString = "";
+
+//        generatePermutationsString(allStringsString, resultString, 0, currentString);
+//        System.out.println(Arrays.toString(resultString.toArray()));
+    }
+
+    public static void generateChordsList(List<List<Integer>> lists, List<Integer[]> result, int depth, List<Integer> current) {
+        if (depth == lists.size()) {
+            result.add(current.toArray(new Integer[0]));
+
+            return;
+        }
+
+        for (int i = 0; i < lists.get(depth).size(); i++) {
+            generateChordsList(lists, result, depth + 1, submitCurrent(lists, current, depth, i));
+        }
+    }
+
+    public static List<Integer> submitCurrent(List<List<Integer>> lists, List<Integer> current, int depth, int i) {
+        List<Integer> currentCopy = current;
+        currentCopy.add(lists.get(depth).get(i));
+        return currentCopy;
+    }
+
+
+    public static void generateChordsString(List<List<String>> lists, List<String> result, int depth, String current) {
         if (depth == lists.size()) {
             result.add(current);
             return;
         }
 
         for (int i = 0; i < lists.get(depth).size(); i++) {
-            current.add(lists.get(depth).get(i));
-            generatePermutations(lists, result, depth + 1, current);
+            generateChordsString(lists, result, depth + 1, current + lists.get(depth).get(i));
         }
     }
-
-
 
 
     }
